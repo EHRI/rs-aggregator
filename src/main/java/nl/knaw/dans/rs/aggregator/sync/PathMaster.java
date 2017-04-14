b057519753ad712dd590420d6ca48d6cf3b343a0
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2017-04-14 19:17:49 +0200

    Define paths wuth PathMaster

---
+++ src/main/java/nl/knaw/dans/rs/aggregator/sync/PathMaster.java
@@ -0,0 +1,7 @@
+package nl.knaw.dans.rs.aggregator.sync;
+
+/**
+ * Created on 2017-04-13 09:34.
+ */
+public class PathMaster {
+}
---
+++ src/test/java/nl/knaw/dans/rs/aggregator/sync/PathMasterTest.java
@@ -0,0 +1,7 @@
+package nl.knaw.dans.rs.aggregator.sync;
+
+/**
+ * Created on 2017-04-13 09:55.
+ */
+public class PathMasterTest {
+}
