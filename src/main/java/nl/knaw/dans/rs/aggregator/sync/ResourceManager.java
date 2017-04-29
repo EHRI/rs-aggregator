df5b67e8a2648eb14cd722f28c72297002c2bfe0
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2017-04-29 16:36:38 +0200

    Javadoced ResourceManager

--- src/main/java/nl/knaw/dans/rs/aggregator/sync/ResourceManager.java
+++ src/main/java/nl/knaw/dans/rs/aggregator/sync/ResourceManager.java
@@ -9,10 +9,23 @@ import java.util.Set;
  * A ResourceManager manages resources and is capable of answering simple questions about the managed resources.
  * Resources are indicated by their normalized {@link java.net.URI}.
  *
+ * For each set of resources the accompanying {@link PathFinder} will be set on the ResourceManager.
+ * See {@link ResourceManager#setPathFinder(PathFinder)}
+ *
  * @see nl.knaw.dans.rs.aggregator.util.NormURI#normalize(URI)
  */
 public interface ResourceManager {
 
+  /**
+   * Sets the current pathFinder on this ResourceManager. The pathFinder may be useful, even if resources
+   * are not managed on the file system. For instance the URI of the capabilityList that defines the
+   * current set of resources can be found with {@link PathFinder#getCapabilityListUri()}.
+   *
+   * A ResourceManager should rely on the fact that each call to this method signifies the
+   * synchronisation of a new set of resources.
+   *
+   * @param pathFinder the pathFinder for the current set of resources
+   */
   void setPathFinder(PathFinder pathFinder);
 
   /**
@@ -52,14 +65,54 @@ public interface ResourceManager {
    */
   VerificationStatus verifySize(@Nonnull URI normalizedURI, long size);
 
+  /**
+   * Clean up the managed store. Only resources that are identified by the given <code>normalizedURIs</code>
+   * should be kept in the managed store, others should be removed or deleted.
+   *
+   * This call enables the ResourceManager to clean up if the Source issues a new ResourceList. The ResourceManager
+   * should only remove resources that are not listed. The ResourceManager will be enabled
+   * to {@link ResourceManager#create(URI)} and/or {@link ResourceManager#update(URI)} resources that are indicated
+   * in the set but are not yet in the managed store.
+   *
+   * @param normalizedURIs identify the resources that should be kept in the managed store.
+   * @return <code>true</code> if the operation succeeded, <code>false</code> otherwise
+   */
   boolean keepOnly(@Nonnull Set<URI> normalizedURIs);
 
+  /**
+   * Keep the resource that is identified by the given <code>normalizedURI</code>. The resource is present
+   * and verified to be up-to-date. So the ResourceManager is not expected to handle the indicated resource,
+   * but may, of course, take any action it deems appropriate.
+   *
+   * @param normalizedURI identifies the resource
+   * @return <code>true</code> if the operation succeeded, <code>false</code> otherwise
+   */
   boolean keep(@Nonnull URI normalizedURI);
 
+  /**
+   * Create the resource that is identified by the given <code>normalizedURI</code>. The resource was not present.
+   *
+   * @param normalizedURI identifies the resource
+   * @return <code>true</code> if the operation succeeded, <code>false</code> otherwise
+   */
   boolean create(@Nonnull URI normalizedURI);
 
+  /**
+   * Update the resource that is identified by the given <code>normalizedURI</code>. The resource was present but
+   * was not up-to-date.
+   *
+   * @param normalizedURI identifies the resource
+   * @return <code>true</code> if the operation succeeded, <code>false</code> otherwise
+   */
   boolean update(@Nonnull URI normalizedURI);
 
+  /**
+   * Delete or remove the resource that is identified by the given <code>normalizedURI</code>. The resource
+   * was present but was purged from the set of resources.
+   *
+   * @param normalizedURI identifies the resource
+   * @return <code>true</code> if the operation succeeded, <code>false</code> otherwise
+   */
   boolean delete(@Nonnull URI normalizedURI);
 
 }
